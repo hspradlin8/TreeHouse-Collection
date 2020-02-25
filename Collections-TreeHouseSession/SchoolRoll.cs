6e73292bf203c98f95d1035c7d913fd0d9df3a1c
Heidi <hspradlin8@gmail.com>
2020-02-24 21:37:30 -0600

    firstCommit

---
+++ Collections-TreeHouseSession/SchoolRoll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Treehouse
+{
+    class SchoolRoll
+    {
+        private List<Student> _students = new List<Student>();
+
+        public IEnumerable<Student> Students { get { return _students; } }
+
+        public void AddStudents(IEnumerable<Student> students)
+            {
+                _students.AddRange(students);
+            }
+    }
+}
---
+++ Collections-TreeHouseSession/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace Treehouse
+{
+    internal class Student
+    {
+        public string Name { get; internal set; }
+        public int GradeLevel { get; internal set; }
+    }
+}
